d5ac99ec5951ed5da9eb2c8f90a15ab41039c8d8
Jose <josedlopez20@icloud.com>
2020-02-24 20:29:09 +0100

    Chat view and controller

---
+++ petitui/petitui/ChatController/Message.swift
@@ -0,0 +1,39 @@
+//
+//  Message.swift
+//  petitui
+//
+//  Created by Jose D. on 20/02/2020.
+//  Copyright © 2020 Sergio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MessageKit
+
+struct Member {
+  let name: String
+  let color: UIColor
+}
+
+struct Message {
+  let member: Member
+  let text: String
+  let messageId: String
+}
+
+extension Message: MessageType {
+  var sender: SenderType {
+    return Sender(senderId: member.name, displayName: member.name)
+  }
+  
+  var sentDate: Date {
+    return Date()
+  }
+  
+  var kind: MessageKind {
+    return .text(text)
+  }
+}
+    
+  
+
---
+++ petitui/petitui/DataModels/MessageResponse.swift
@@ -0,0 +1,43 @@
+//
+//  MessageResponse.swift
+//  petitui
+//
+//  Created by Jose D. on 20/02/2020.
+//  Copyright © 2020 Sergio. All rights reserved.
+//
+
+import Foundation
+
+struct MessageResponse : Codable {
+
+    var code: Int?
+    var msg, errorMsg: String?
+    var messages: [ChatMessage]?
+    
+
+    enum CodingKeys: String, CodingKey {
+        case code
+        case errorMsg = "error_msg"
+        case msg
+        case messages
+    }
+    
+}
+
+struct ChatMessage : Codable {
+
+    var message:String
+    var id:Int
+    var chatId:Int
+    var owner:Int
+
+    enum CodingKeys: String, CodingKey {
+     
+
+        case message
+        case chatId = "chat_id"
+        case owner = "id_owner_message"
+        case id
+    }
+    
+}
