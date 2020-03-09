97386dd45d343e84429d94ab8680b7ae24c467ca
Aitor <aitor.ballesteros.pavon@gmail.com>
2020-03-09 16:40:52 +0100

    Add user session in location

--- petitui/petitui/ProfileViewController.swift
+++ petitui/petitui/ProfileViewController.swift
@@ -12,17 +12,21 @@ class ProfileViewController: UIViewController, MyDataSendingDelegateProtocol  {
     
     public var latitude : Double?
     public var longitude: Double?
-
+    var user:User?
+    
     func sendDataToRegisterVC(latitude: Double, longitude: Double) {
         self.latitude = latitude
         self.longitude = longitude
     }
     
-      @IBAction func saveChanges(_ sender: Any) {
+    @IBAction func saveChanges(_ sender: Any) {
         
-        if latitude != nil && longitude != nil {
-            ApiManager.updateLatLongUser(id: 1, latitude: latitude!, longitude: longitude!){
-                      response in print("Response updated Latitude and Longitude",response)
+        if latitude != nil && longitude != nil  {
+            if let userId = user?.id{
+                ApiManager.updateLatLongUser(id: userId, latitude: latitude!, longitude: longitude!){
+                    response in print("Response updated Latitude and Longitude",response)
+                }
+                
             }
         }else{
             self.present(DataHelpers.displayAlert(userMessage: "The changes haven't been updated", alertType: 0), animated: true, completion: nil)
@@ -31,29 +35,28 @@ class ProfileViewController: UIViewController, MyDataSendingDelegateProtocol  {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            if segue.identifier == "toSearchLocation"{
-                let searchVC : SearchLocationViewController = segue.destination as! SearchLocationViewController
-                searchVC.delegate = self
-            }
+        if segue.identifier == "toSearchLocation"{
+            let searchVC : SearchLocationViewController = segue.destination as! SearchLocationViewController
+            searchVC.delegate = self
+        }
     }
-
     
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let decoded  = UserDefaults.standard.object(forKey: "user")
+        do {
+            user = try JSONDecoder().decode(User.self, from: decoded as! Data)
+            
+        }
+        catch  {
+            
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
