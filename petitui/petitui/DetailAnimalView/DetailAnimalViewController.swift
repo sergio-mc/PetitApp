e98a56c5d007c224ca1602801be3ceac3774273e
Sergio Marrón Calle <sergiodev0106@gmail.com>
2020-03-10 18:54:13 +0100

    Dinamic profile and user favorites. Some fixes

--- petitui/petitui/DetailAnimalView/DetailAnimalViewController.swift
+++ petitui/petitui/DetailAnimalView/DetailAnimalViewController.swift
@@ -49,9 +49,9 @@ class DetailAnimalViewController: UIViewController, UIImagePickerControllerDeleg
             print("Favorite created")
             
         }
-
         
-    
+        
+        
         
     }
     
@@ -62,16 +62,16 @@ class DetailAnimalViewController: UIViewController, UIImagePickerControllerDeleg
             setOwnerData(pet:pet) 
         }
         let decoded  = UserDefaults.standard.object(forKey: "user")
-            do {
-                user = try JSONDecoder().decode(User.self, from: decoded as! Data)
-                print("Soy Usuario: ", user!)
-                getFavoriteByUser(idUser: user!.id!, idAnimal: detailPetID!)
-                
-                
-            }
-            catch  {
-                
-            }
+        do {
+            user = try JSONDecoder().decode(User.self, from: decoded as! Data)
+            
+            getFavoriteByUser(idUser: user!.id!, idAnimal: detailPetID!)
+            
+            
+        }
+        catch  {
+            
+        }
         
         
         
@@ -188,4 +188,6 @@ class DetailAnimalViewController: UIViewController, UIImagePickerControllerDeleg
         
     }
     
+    
 }
+
---
+++ petitui/petitui/ProfileController/ProfileViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ProfileViewController.swift
+//  petitui
+//
+//  Created by Sergio on 09/03/2020.
+//  Copyright © 2020 Sergio. All rights reserved.
+//
+import UIKit
+
+class ProfileViewController: UIViewController{
+    
+    @IBAction func userPets(_ sender: Any) {
+        self.segueUserPets()
+    }
+    
+    var user: User?
+    
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var userLocation: UILabel!
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+        
+        let decoded  = UserDefaults.standard.object(forKey: "user")
+        self.userImage.image = UIImage(imageLiteralResourceName: "dog")
+        do {
+            user = try JSONDecoder().decode(User.self, from: decoded as! Data)
+            if let userPicture = user?.picture{
+                ApiManager.getImage(url: userPicture){
+                    
+                    picture in
+                    self.userImage.image = UIImage(data: picture)
+                    
+                }
+            }
+            
+            self.userName.text = user?.userName
+            self.userEmail.text = user?.email
+            self.userLocation.text = "Location"
+        }
+        catch  {
+            
+        }
+        
+    }
+    
+    func segueUserPets()  {
+        performSegue(withIdentifier: "userPets", sender: nil)
+    }
+}
+
